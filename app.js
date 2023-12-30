1aa5053e906dd57eaed37e240bbbd670cfe73f52
Dani Ren <daniren@Danis-Air-2.hitronhub.home>
2023-12-30 14:40:43 -0800

    Fix criteria for drop down to disappear

--- app.js
+++ app.js
@@ -16,42 +16,42 @@ menu.addEventListener('click', menuOptions);
 // show active menu when scrolling (underline in the navbar)
 // target all of the menu
 const highlightMenu = () => {
-const elem = document.querySelector('.scroll')
-const homeMenu = document.querySelector('#home-page')
-const aboutMenu = document.querySelector('#about-page')
-const projectsMenu = document.querySelector('#projects-page')
+    const elem = document.querySelector('.scroll')
+    const homeMenu = document.querySelector('#home-page')
+    const aboutMenu = document.querySelector('#about-page')
+    const projectsMenu = document.querySelector('#projects-page')
 
-// scroll position that shows where we are actually positioned at
-let scrollPos = window.scrollY
+    // scroll position that shows where we are actually positioned at
+    let scrollPos = window.scrollY
 
-// add the scroll class to menu items
-if (window.innerWidth > 960 && scrollPos < 550) {
-    homeMenu.classList.add('scroll') 
-    aboutMenu.classList.remove('scroll') 
-    return 
-} else if (window.innerWidth > 960 && scrollPos < 1200) {
-    aboutMenu.classList.add('scroll')
-    homeMenu.classList.remove('scroll')
-    projectsMenu.classList.remove('scroll')
-    return
-} else if (window.innerWidth > 960 && scrollPos < 2400) {
-aboutMenu.classList.remove('scroll')
-projectsMenu.classList.add('scroll')
-return
-}
+    // add the scroll class to menu items
+    if (window.innerWidth > 960 && scrollPos < 550) {
+        homeMenu.classList.add('scroll')
+        aboutMenu.classList.remove('scroll')
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 1200) {
+        aboutMenu.classList.add('scroll')
+        homeMenu.classList.remove('scroll')
+        projectsMenu.classList.remove('scroll')
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 2400) {
+        aboutMenu.classList.remove('scroll')
+        projectsMenu.classList.add('scroll')
+        return
+    }
 
-if ((elem && window.innerWidth < 960 && scrollPos < 550) || elem) {
-    elem.classList.remove('scroll');
-}
+    if ((elem && window.innerWidth < 960 && scrollPos < 550) || elem) {
+        elem.classList.remove('scroll');
+    }
 }
 
 window.addEventListener('scroll', highlightMenu)
 window.addEventListener('click', highlightMenu)
 
-// remove the drop down menu as soon as an option
+// remove the drop down menu as soon as an option is clicked
 const hideDropDown = () => {
     const menuOptions = document.querySelector('.is-active')
-    if (window.innerWidth <= 768 && menuOptions) {
+    if (window.innerWidth <= 800 && menuOptions) {
         menu.classList.toggle('is-active')
         menuLinks.classList.remove('active')
     }
